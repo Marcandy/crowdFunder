5ebd6996464f8e6abac15dc70743116bec74057e
marcandy <marcandy.nj@gmail.com>
2017-01-20 12:57:50 -0700

    structure

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import path from 'path'
 
 let app = express();
 
 app.get('/*', (req, res) => {
-  res.send('hello world');
+  res.sendFile(path.join(__dirname, './index.html'));
 })
 
 app.listen(3000, () => console.log('Running localhost 3000'))
