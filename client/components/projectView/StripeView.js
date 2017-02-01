c74f5d4948bd0fcbb5e203b89c1a98bfc437e058
marcandy <marcandy.nj@gmail.com>
2017-01-31 19:16:45 -0700

    progress

--- client/components/projectView/StripeView.js
+++ client/components/projectView/StripeView.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {connect } from 'react-redux';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 export default class StripeView extends React.Component {
   constructor(props) {
@@ -7,20 +9,33 @@ export default class StripeView extends React.Component {
 
     // this.charge = axios.post()
   }
-
+  onToken(token) {
+  axios.post('/api/charge', {
+    method: 'POST',
+    body: JSON.stringify(token),
+  }).then(token => {
+    alert(`We are in business, ${token.email}`);
+  });
+}
   render() {
+
     return (
-      <form action="/your-server-side-code" method="POST">
-        <script
-          src="https://checkout.stripe.com/checkout.js" className="stripe-button"
-          data-key="pk_test_J3VU4fHBt40EOdPjkWytAG2W"
-          data-amount="2000"
-          data-name="Demo Site"
-          data-description="2 widgets"
-          data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-          data-locale="auto">
-        </script>
-      </form>
+      // <form action="/your-server-side-code" method="POST">
+      //   <script
+      //     src="https://checkout.stripe.com/checkout.js" className="stripe-button"
+      //     data-key="pk_test_J3VU4fHBt40EOdPjkWytAG2W"
+      //     data-amount="2000"
+      //     data-name="Demo Site"
+      //     data-description="2 widgets"
+      //     data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+      //     data-locale="auto">
+      //   </script>
+      // </form>
+
+      <StripeCheckout
+        token={this.onToken}
+        stripeKey="pk_test_J3VU4fHBt40EOdPjkWytAG2W"
+      />
     )
   }
 }
--- server/controllers/stripeCtrl.js
+++ server/controllers/stripeCtrl.js
@@ -6,22 +6,82 @@ var stripe = require('stripe')(config.stripe.testKey);
 module.exports = {
   Charge: function(req, res,next) {
   var stripeToken = req.body.stripeToken;
-  var amount = req.body.price * 100;
-
+  var amount = 200 * 100;
+console.log('stripe');
   // ensure amount === actual product amount to avoid fraud
+console.log(req.body);
+  stripe.customers.create({
+     email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  })
+  .then(function (customer) {
+    stripe.charges.create({
+      amount,
+      description: "Sample Charge",
+         currency: "usd",
+         customer: customer.id,
+          card: stripeToken,
+    })
+  }, function(err, charge) {
+      if (err) {
+        console.log(err);
+        res.send('error');
+      } else {
+        res.send('success');
+      }
+    })
+  // .then(function( charge) {
+  //   // if (err) {
+  //   //   console.log(err);
+  //   //   res.send('error');
+  //   // } else {
+  //   console.log(charge);
+  //         res.send('success');
+  //
+  // });
 
-  stripe.charges.create({
-    card: stripeToken,
-    currency: 'usd',
-    amount: amount
-  },
-  function(err, charge) {
-    if (err) {
-      console.log(err);
-      res.send('error');
-    } else {
-      res.send('success');
-    }
-  });
+  }
 }
-}
+
+// app.post("/charge", (req, res) => {
+//   console.log(req.body);
+//   let amount = 500;
+//
+//   stripe.customers.create({
+//      email: req.body.stripeEmail,
+//     source: req.body.stripeToken
+//   })
+//   .then(customer =>
+//     stripe.charges.create({
+//       amount,
+//       description: "Sample Charge",
+//          currency: "usd",
+//          customer: customer.id
+//     }))
+//   .then(charge => res.render("charge.pug"));
+// });
+
+
+//--------------before
+// module.exports = {
+//   Charge: post('/charge', function(req, res,next) {
+//   var stripeToken = req.body.stripeToken;
+//   var amount = req.body.price * 100;
+//
+//   // ensure amount === actual product amount to avoid fraud
+//
+//   stripe.charges.create({
+//     card: stripeToken,
+//     currency: 'usd',
+//     amount: amount
+//   },
+//   function(err, charge) {
+//     if (err) {
+//       console.log(err);
+//       res.send('error');
+//     } else {
+//       res.send('success');
+//     }
+//   });
+// });
+// }
