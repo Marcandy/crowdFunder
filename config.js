a39df5fec09ed73d918239fb6fa9d373aeced2e8
marcandy <marcandy.nj@gmail.com>
2017-02-08 19:09:23 -0700

    Changes for heroku

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  connectionString : "postgres://massive:Forces$57@localhost/crowdFund",
+  secret: 'adjlfdljlafj$alfja&sdkfj9$fjldsajfZ',
+  google: {
+    clientID: '544815591077-2rbo838gq6ikfkkhr4qah8iob9533cjc.apps.googleusercontent.com',
+    clientSecret: 'M4-vw84QTYYsDjX1SeWWNhqw'
+  },
+
+  stripe: {
+    testKey: 'sk_test_CYHG7Sg19uyLrHQxI0xk6gdH',
+    keyPublishable: 'pk_test_J3VU4fHBt40EOdPjkWytAG2W'
+  }
+}
